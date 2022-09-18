d469ff2b5d51c389d6bc5f3ccf65a9db7bb10ca6
rishabhreng <rishabhreng@gmail.com>
2022-09-18 08:41:13 -0500

    better gitignore

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.scoutingapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.zxing;
+    requires javafx.graphics;
     requires com.google.zxing.javase;
 
     opens com.scoutingapp to javafx.fxml;
