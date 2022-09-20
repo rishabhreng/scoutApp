dfe238e8d17895c22b6d49d3eb093fb582b0f77e
rishabhreng <rishabhreng@gmail.com>
2022-09-20 16:54:39 -0500

    more pages

--- src/main/java/com/scoutingapp/SceneController.java
+++ src/main/java/com/scoutingapp/SceneController.java
@@ -2,25 +2,23 @@ package com.scoutingapp;
 
 //TODO add QR functionality to sendInfo()
 
-import com.google.zxing.qrcode.QRCodeWriter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
-import javafx.scene.image.Image;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
-
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 public class SceneController {
 
@@ -37,17 +35,30 @@ public class SceneController {
     @FXML private LimitedTextField mn; //match number
     @FXML private LimitedTextField sln; //scouter last name
     @FXML private ComboBox<String> rp; //robot position
-    @FXML private ComboBox<String> ml;
-    @FXML private CheckBox cp;
+    @FXML private ComboBox<String> ml; //match level
+    @FXML private CheckBox cp; //cargo preload
 //page 2
-    @FXML private Text ca;
+    @FXML private Text ca, ucsa, lcsa, cmda; //cargo acquired, ucargo, lcargo, cargodropped
+    @FXML private CheckBox ta; //taxied
+//page 3
+    @FXML private Text ucst, lcst, cmdt; //ucargo, lcargo, cargodropped
+//page4
+    @FXML private CheckBox cla;
+    @FXML private ComboBox<String> cl;
+//page5
+    @FXML private CheckBox dt;
+    @FXML private ComboBox<String> de;
+    @FXML private ComboBox<String> df;
+    @FXML private TextField co;
+    @FXML private Text f;
 
-//page 6
+    //page 6
     @FXML private ImageView imageBox;
 
     //used for changing pages
     private static int sceneIndex = 0;
     private static int codeNum;
+
     //stores user input data
     private static HashMap<String, String> info = new HashMap<>();
 
@@ -87,23 +98,44 @@ public class SceneController {
                 info.put("ml", ml.getValue());
                 info.put("cp", String.valueOf(cp.isSelected()));
                 info.put("rp", rp.getValue());
-//                System.out.println(Arrays.toString(info.entrySet().toArray()));
                 break;
             case 2:
                 info.put("ca",ca.getText());
-//                System.out.println(Arrays.toString(info.entrySet().toArray()));
+                info.put("ucsa", ucsa.getText());
+                info.put("lcsa", lcsa.getText());
+                info.put("cmda", cmda.getText());
+                info.put("ta", String.valueOf(ta.isSelected()));
+                break;
+            case 3:
+                info.put("ucst", ucst.getText());
+                info.put("lcst", lcst.getText());
+                info.put("cmdt", cmdt.getText());
+                break;
+            case 4:
+                info.put("cla", String.valueOf(cla.isSelected()));
+                info.put("cl", cl.getValue());
+                break;
+            case 5:
+                info.put("dt", String.valueOf(dt.isSelected()));
+                info.put("df", df.getValue());
+                info.put("de", de.getValue());
+                info.put("co", co.getText());
+                info.put("f", f.getText());
                 break;
             default:
-//                System.out.println(Arrays.toString(info.entrySet().toArray()));
+                System.out.println("default case");
         }
+                System.out.println(Arrays.toString(info.entrySet().toArray()));
     }
 
     //don't edit
     @FXML public void goToNextPage(ActionEvent event) throws IOException {
+        System.out.println("page number is " + sceneIndex);
         collectData();
-        if (sceneIndex >=4) sceneIndex = 0;
+        if (sceneIndex >= 7) sceneIndex = 0;
         else sceneIndex++;
         setPage(event);
+        System.out.println("new page number is " + sceneIndex);
     }
     @FXML public void goToPrevPage(ActionEvent event) throws IOException {
         collectData();
@@ -129,10 +161,33 @@ public class SceneController {
     }
 
     //don't edit
-    public void increment(Text text) {text.setText(String.valueOf(Integer.parseInt(text.getText())+1));}
-    public void decrement(Text text) {if(!text.getText().equals("0")) text.setText(String.valueOf(Integer.parseInt(text.getText())-1));}
+    public void increment(Text text) {
+        text.setText(String.valueOf(Integer.parseInt(text.getText())+1));}
+    public void decrement(Text text) {
+        if(!text.getText().equals("0")) text.setText(String.valueOf(Integer.parseInt(text.getText())-1));}
 
     //add more of these when you add more "incrementer/decrementer button" elements for text elements
     @FXML public void incrementCA(ActionEvent actionEvent) {increment(ca);}
     @FXML public void decrementCA(ActionEvent actionEvent) {decrement(ca);}
+
+    @FXML public void incrementUCSA(ActionEvent actionEvent) {increment(ucsa);}
+    @FXML public void decrementUCSA(ActionEvent actionEvent) {decrement(ucsa);}
+
+    @FXML public void incrementLCSA(ActionEvent actionEvent) {increment(lcsa);}
+    @FXML public void decrementLCSA(ActionEvent actionEvent) {decrement(lcsa);}
+
+    @FXML public void incrementCMDA(ActionEvent actionEvent) {increment(cmda);}
+    @FXML public void decrementCMDA(ActionEvent actionEvent) {decrement(cmda);}
+
+    @FXML public void incrementUCST(ActionEvent actionEvent) {increment(ucst);}
+    @FXML public void decrementUCST(ActionEvent actionEvent) {decrement(ucst);}
+
+    @FXML public void incrementLCST(ActionEvent actionEvent) {increment(lcst);}
+    @FXML public void decrementLCST(ActionEvent actionEvent) {decrement(lcst);}
+
+    @FXML public void incrementCMDT(ActionEvent actionEvent) {increment(cmdt);}
+    @FXML public void decrementCMDT(ActionEvent actionEvent) {decrement(cmdt);}
+
+    @FXML public void incrementF(ActionEvent actionEvent) {increment(f);}
+    @FXML public void decrementF(ActionEvent actionEvent) {decrement(f);}
 }
